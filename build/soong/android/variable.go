5f2b2ab332150f618d8e058b544b8189e2ab3558
Sagar Rokade <sagarrokade006@gmail.com>
2018-11-16 16:22:48 +0000

    Revert "vendor: Clean up soong flags"
    This reverts commit fca9ad070f62775230cb851f0fa4997f81bc82c6.

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -8,40 +8,70 @@ type Product_variables struct {
 		Cflags []string
 	}
 
-	Needs_text_relocations struct {
+        Needs_legacy_camera_hal1_dyn_native_handle struct {
 		Cppflags []string
 	}
 
-	Target_shim_libs struct {
+	Uses_media_extensions struct {
+		Cflags []string
+	}
+
+	BoardUsesQTIHardware struct {
+		Cflags []string
 		Cppflags []string
 	}
+
+	BoardUsesQCOMHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	TargetUsesQCOMBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	TargetUsesQCOMLegacyBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	BoardUsesLegacyAlsa struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	Cant_reallocate_omx_buffers struct {
+		Cflags []string
+        }
 
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
-
-	Uses_nvidia_enhancements struct {
+	
+		Target_shim_libs struct {
 		Cppflags []string
-	}
-
-	Uses_qcom_bsp_legacy struct {
-		Cppflags []string
-	}
-
-	Uses_qti_camera_device struct {
-		Cppflags []string
-		Shared_libs []string
 	}
 }
 
 type ProductVariables struct {
+<<<<<<< HEAD
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+=======
+>>>>>>> parent of fca9ad0... vendor: Clean up soong flags
 	Needs_text_relocations  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Target_shim_libs  *string `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Uses_nvidia_enhancements  *bool `json:",omitempty"`
-	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
-	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+        Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
+	Uses_media_extensions   *bool `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
+	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
+	Target_shim_libs  *string `json:",omitempty"`
 }
