67c8a548a3b63a0cbd77e277f21f8282f8d9f1ad
Sagar Rokade <sagarrokade006@gmail.com>
2018-11-16 16:14:09 +0000

    Revert "Fruity pebbles [2/4]"
    This reverts commit e1fd87ca7dad6289319e8ab0ca2c7411ec6b0428.

--- config/common.mk
+++ config/common.mk
@@ -62,63 +62,6 @@ PRODUCT_PACKAGES += \
     MarkupGoogle \
     WellbeingPrebuilt
 
-# Themes
-# Accents
-PRODUCT_PACKAGES += \
-    PixelTheme \
-    AmberAccent \
-    BlueAccent \
-    BlueGreyAccent \
-    BrownAccent \
-    CyanAccent \
-    CandyRedAccent \
-    DeepOrangeAccent \
-    DeepPurpleAccent \
-    ExtendedGreenAccent \
-    GreenAccent \
-    GreyAccent \
-    IndigoAccent \
-    JadeGreenAccent \
-    LightBlueAccent \
-    LightGreenAccent \
-    LimeAccent \
-    OrangeAccent \
-    PaleBlueAccent \
-    PaleRedAccent \
-    PinkAccent \
-    PurpleAccent \
-    RedAccent \
-    TealAccent \
-    YellowAccent
-
-# UI Variants
-PRODUCT_PACKAGES += \
-    SystemDarkTheme \
-    SettingsDarkTheme \
-    SystemUIDarkTheme \
-    SystemBlackTheme \
-    SettingsBlackTheme \
-    SystemUIBlackTheme \
-    SystemExtendedTheme \
-    SettingsExtendedTheme \
-    SystemUIExtendedTheme \
-    SystemChocolateTheme \
-    SettingsChocolateTheme \
-    SystemUIChocolateTheme \
-    ContactsThemeBlack \
-    DialerThemeBlack \
-    FilesThemeBlack \
-    ContactsThemeDark \
-    DialerThemeDark \
-    FilesThemeDark \
-    ContactsThemeChocolate \
-    DialerThemeChocolate \
-    FilesThemeChocolate \
-    ContactsThemeExtended \
-    DialerThemeExtended \
-    FilesThemeExtended \
-
-
 # Markup libs
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
